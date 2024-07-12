4ec032d952c88e0889d9d4d3fc5b65ca70101a43
Justice Ogbonna <justiceo@users.noreply.github.com>
2024-07-12 13:48:56 -0700

    Clean up package

---
+++ esbuild.js
@@ -0,0 +1,49 @@
+import esbuild from "esbuild";
+
+const ESM_REQUIRE_SHIM = `#!/usr/bin/env node
+
+await (async () => {
+  const { dirname } = await import("path");
+  const { fileURLToPath } = await import("url");
+
+  /**
+   * Shim entry-point related paths.
+   */
+  if (typeof globalThis.__filename === "undefined") {
+    globalThis.__filename = fileURLToPath(import.meta.url);
+  }
+  if (typeof globalThis.__dirname === "undefined") {
+    globalThis.__dirname = dirname(globalThis.__filename);
+  }
+  /**
+   * Shim require if needed.
+   */
+  if (typeof globalThis.require === "undefined") {
+    const { default: module } = await import("module");
+    globalThis.require = module.createRequire(import.meta.url);
+  }
+})();
+`;
+
+/** Whether or not you're bundling. */
+const bundle = true;
+
+/** Tell esbuild to add the shim to emitted JS. */
+const shimBanner = {
+  js: ESM_REQUIRE_SHIM,
+};
+
+/**
+ * ESNext + ESM, bundle: true, and require() shim in banner.
+ */
+const buildOptions = {
+  entryPoints: ["./src/multi-tasks.ts"],
+  outdir: "./dist",
+  format: "esm",
+  target: "esnext",
+  platform: "node",
+  banner: shimBanner,
+  bundle: true,
+  minify: true,
+};
+esbuild.build(buildOptions);
---
+++ src/multi-tasks.ts
@@ -0,0 +1,150 @@
+import { TaskManager, addMessage, Task, BaseTask } from "./cli-tasks";
+
+class StreamTask extends BaseTask {
+  text = "";
+  initialMessage: string = "Stream Task";
+  stream(text: string) {
+    this.text += text;
+    this.updateFn(this.text);
+  }
+}
+
+class Logger extends BaseTask {
+  initialMessage: string = "Logger";
+  log(msg: string) {
+    this.updateFn(msg);
+  }
+}
+function logIt(msg: string) {
+  addMessage(msg);
+}
+
+logIt("Task 1");
+logIt("Task 2");
+logIt("Task 3");
+
+///// Tests /////
+function sleep(seconds: number, signal?: AbortSignal): Promise<void> {
+  if (signal?.aborted) return Promise.resolve();
+
+  return new Promise((resolve, reject) => {
+    const timeout = setTimeout(resolve, seconds * 1000);
+    if (signal) {
+      signal.addEventListener("abort", () => {
+        clearTimeout(timeout);
+        resolve();
+      });
+    }
+  });
+}
+
+function test() {
+  const s2 = new StreamTask("Stream Task 2");
+  const initialTasks: Task[] = [
+    {
+      initialMessage: "Task 1",
+      index: 1000,
+      task: async (message, signal) => {
+        await sleep(2, signal);
+        // taskManager.stop();
+        message("Task 1 is halfway done");
+        await sleep(2, signal);
+        message("Task 1 completed successfully");
+      },
+    },
+    {
+      initialMessage: "Task 2: Empty Task",
+      task: async (message) => {},
+    },
+    {
+      initialMessage: "Task Disabled",
+      disabled: true,
+      task: async (message, signal) => {
+        await sleep(1, signal);
+        message("This task should not run");
+        await sleep(2, signal);
+        message("Disabled Task finished");
+      },
+    },
+    s2,
+  ];
+
+  s2.stream("Stream Task 2 is running");
+
+  const taskManager = TaskManager.getInstance();
+  taskManager.add(...initialTasks);
+
+  // Start the spinner and get the promise
+  const allTasksPromise = taskManager.run();
+
+  // Add a new task after 1 second
+  taskManager.add({
+    initialMessage: "Task 3 with custom symbol",
+    statusSymbol: "#",
+    task: async (message, signal) => {
+      await sleep(1, signal);
+      message("Task 3 is running");
+      const s3 = new StreamTask("Stream Task 3");
+      taskManager.add(s3);
+      s3.stream("Stream Task 3 is running");
+      await sleep(2, signal);
+      s3.close("Stream Task 3 completed");
+      message("Task 3 completed");
+    },
+  });
+
+  const interval = setInterval(() => {
+    s2.stream("\nLorem ipsum dolor sit amet, consectetur adipiscing elit.");
+  }, 500);
+  setTimeout(() => {
+    clearInterval(interval);
+    s2.close("Finished");
+  }, 3000);
+
+  // Add another task after 2 seconds with a specific index
+  setTimeout(() => {
+    taskManager.add({
+      initialMessage: "Task 4 with failure",
+      task: async (message, signal) => {
+        await sleep(1, signal);
+        message("Task with failure is executing");
+        await sleep(1, signal);
+        throw new Error("Random error");
+      },
+    });
+  }, 1000);
+
+  setTimeout(() => {
+    taskManager.add({
+      initialMessage: "Task 5 added late",
+      task: async (message, signal) => {
+        await sleep(1, signal);
+        message("Task 5 is executing");
+        await sleep(1, signal);
+        message("Task 5 is done");
+      },
+    });
+  }, 2500);
+
+  // Try to add a task after all tasks are completed (should have no effect)
+  setTimeout(() => {
+    taskManager.add({
+      initialMessage: "Task Too late (Should not be added)",
+      task: async (message, signal) => {
+        await sleep(1, signal);
+        message("This task should not run");
+      },
+    });
+  }, 6000);
+
+  // Wait for all tasks to complete
+  allTasksPromise
+    .then(() => {
+      console.log("\nAll tasks completed!");
+    })
+    .catch((error) => {
+      console.error("\nAn error occurred:", error);
+    });
+}
+
+test();
